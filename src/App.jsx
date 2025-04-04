2e3f6ab2111302dae15afe4b2a099fc0d94ffe98
matheus031425 <rm95390@estudante.fieb.edu.br>
2025-04-04 09:55:58 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,51 @@
-import{useState} from "react";
+import { useState } from "react";
+import poke from './assets/pokeapi_256.png'
  
 function App() {
  
-  const [nome, setNome] = useState("Jesus")
- 
-  // var nome = "Victor";
+const Formulario = (props) =>{
 
-  const Formulario = () => {
-    return (
-      <div>
-      <input
-        className="nome"
-        name ="nome"
-        onChange={(e)=> {setNome(e.target.value)}}
-        placeholder="Digite um nome"
-        type="text" />
-      <button
-        className="Botao"
-        onClick={() => { alert(nome) }}>
-        CLIQUE AQUI
-      </button>
-      </div>
-    )
-  }
+const [nome, setNome] = useState("Memphis")
+ 
  
   return (
     <div>
-      <h3>Pizzaria 2F</h3>
-      <Formulario />
-      <Formulario />
-      <Formulario />
+      <h3></h3>
+ 
+ 
+      <input
+      className="nome"
+      name = "nome"
+      onChange={(e)=>{setName(e.target.value)}}
+      placeholder={props.sombra !=null ? props.sombra : "texto padrão"}
+      type="text" />
+ 
+ 
+ 
+      <button
+      className="botao"
+      onClick={()=>{alert(nome)}}>
+      CLIQUE AQUI
+      </button>
     </div>
-
-   )
+  )
 }
+ 
+return (
+ 
+ 
+<div>
+  <h3>Pizzaria 2F</h3>
+  <p>texto do parágrafo</p>
+  <img src = {poke} style={{width:200, height:100}}/>
+    <Formulario sombra="Digite seu nome.." />
+    <Formulario sombra="Digite seu e-mail..."/>
+    <Formulario sombra="(99)90000-0000"/>
+    <Formulario/>
+    <Formulario/>
+    <Formulario/>
+</div>
+)
+}
+ 
+export default App
