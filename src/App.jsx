d82b93df44ce6ec1f9faf52c3b36ffe074fce7ca
matheus031425 <rm95390@estudante.fieb.edu.br>
2025-09-19 07:47:45 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,14 @@
-import { useState } from "react";
-import poke from './assets/pokeapi_256.png'
- 
-function App() {
- 
-const Formulario = (props) =>{
+import Produtos from "./pages/produtos"
 
-const [nome, setNome] = useState("Memphis")
- 
- 
+const App = () => {
+
+
   return (
-    <div>
-      <h3></h3>
- 
- 
-      <input
-      className="nome"
-      name = "nome"
-      onChange={(e)=>{setName(e.target.value)}}
-      placeholder={props.sombra !=null ? props.sombra : "texto padrão"}
-      type="text" />
- 
- 
- 
-      <button
-      className="botao"
-      onClick={()=>{alert(nome)}}>
-      CLIQUE AQUI
-      </button>
-    </div>
+    <>
+      <h1>Projeto Pizzaria</h1>
+      <Produtos />
+    </>
   )
 }
- 
-return (
- 
- 
-<div>
-  <h3>Pizzaria de goti 2F</h3>
-  <p>texto do parágrafo</p>
-  <img src = {poke} style={{width:200, height:100}}/>
-    <Formulario sombra="Digite seu nome.." />
-    <Formulario sombra="Digite seu e-mail..."/>
-    <Formulario sombra="(99)90000-0000"/>
-    <Formulario/>
-    <Formulario/>
-    <Formulario/>
-</div>
-)
-}
- 
+
 export default App
---
+++ src/pages/produtos/index.jsx
@@ -0,0 +1,15 @@
+const Produtos = () => {
+
+    return (
+        <div>
+            <ul>
+                <li>pizza de muçarela</li>
+                <li>pizza de calabresa</li>
+                <li>pizza de portuguesa</li>
+                <li>pizza de quatro quijo</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Produtos
